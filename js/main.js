deb91f3a856581ba34db74bf84bf45a06fd331c0
Beau Daignault <beau@littlehou.se>
2017-08-29 17:08:19 -0700

    Created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+//fundamentals.generalassemb.ly/v3.1/08_unit/control-flow-assignment.html
 
- console.log("User flipped " + cardOne + " & " + cardTwo);
- console.log("User flipped " + cardThree + " & " + cardTwo);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[3];
+
+cardsInPlay.push(cardOne, cardTwo);
+//cardsInPlay.push('king');
+
+console.log("The user flipped " + cardsInPlay);
+
+ if ( cardsInPlay.length === 2 ) {
+
+	if ( cardsInPlay[0] === cardsInPlay[1] ) { 
+		alert("You've found a match!");
+	} else {
+		alert("Sorry, try again.");
+	} 
+} else {
+	alert("Sorry, try again. Must be " + cardsInPlay.length * 2 + " cards played.");
+}

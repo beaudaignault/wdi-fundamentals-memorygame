0e48d8214f1f28d1ab1d476c45a319468acb872c
Beau Daignault <beau@littlehou.se>
2017-08-31 10:54:25 -0700

    Create first functions

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
-//fundamentals.generalassemb.ly/v3.1/08_unit/control-flow-assignment.html
 
-var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[3];
+var cards = ["queen", "queen", "king", "king"]; // has global scope
+var cardsInPlay = []; // has global scope
+var checkForMatch = function() {
+	if ( cardsInPlay.length === 2 ) {
 
-cardsInPlay.push(cardOne, cardTwo);
-//cardsInPlay.push('king');
-
-console.log("The user flipped " + cardsInPlay);
-
- if ( cardsInPlay.length === 2 ) {
-
-	if ( cardsInPlay[0] === cardsInPlay[1] ) { 
-		alert("You've found a match!");
-	} else {
-		alert("Sorry, try again.");
+		if ( cardsInPlay[0] === cardsInPlay[1] ) { 
+			alert("You've found a match!");
+		} else {
+			alert("Sorry, try again.");
+		} 
 	} 
-} else {
-	alert("Sorry, try again. Must be " + cardsInPlay.length * 2 + " cards played.");
 }
+
+var flipCard = function(cardId) {
+
+	console.log("The user flipped " + cards[cardId]);
+	cardsInPlay.push(cardId);
+	checkForMatch();
+};
+	flipCard(0);
+	flipCard(2);
+

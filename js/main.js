c606ff1c31366a213599d1f9774e76d2e717d3ab
Beau Daignault <beau@littlehou.se>
2017-09-02 16:32:39 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
+var cards = [
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+]; // has global scope
 
-var cards = ["queen", "queen", "king", "king"]; // has global scope
 var cardsInPlay = []; // has global scope
+
 var checkForMatch = function() {
 	if ( cardsInPlay.length === 2 ) {
 
@@ -14,8 +36,10 @@ var checkForMatch = function() {
 
 var flipCard = function(cardId) {
 
-	console.log("The user flipped " + cards[cardId]);
-	cardsInPlay.push(cardId);
+	console.log("The user flipped a " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	checkForMatch();
 };
 	flipCard(0);

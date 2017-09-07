56c96fe9fb524826d153edfa4f80e33398888934
Beau Daignault <beau@littlehou.se>
2017-09-07 11:17:38 -0700

    relocate if cardsInplay to flipCard + add cardsInPlay empty instance per solution

--- js/main.js
+++ js/main.js
@@ -24,33 +24,34 @@ var cards = [
 var cardsInPlay = []; // has global scope
 
 var checkForMatch = function() {
-	if ( cardsInPlay.length === 2 ) {
-
 		if ( cardsInPlay[0] === cardsInPlay[1] ) { 
 			alert("You've found a match!");
 		} else {
 			alert("Sorry, try again.");
 		} 
-	} 
 };
 
 var flipCard = function() {
 	var cardId = this.getAttribute( 'data-id' );
-	console.log("The user flipped a " + cards[cardId].rank);
-	cardsInPlay.push(cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
-	this.setAttribute('src', cards[cardId].cardImage);
-
-	checkForMatch();
+		console.log("The user flipped a " + cards[cardId].rank);
+		cardsInPlay.push(cards[cardId].rank);
+		console.log(cards[cardId].cardImage);
+		console.log(cards[cardId].suit);
+		this.setAttribute('src', cards[cardId].cardImage);
+	
+	if ( cardsInPlay.length === 2 ) {
+		checkForMatch();
+	    cardsInPlay = [];
+    }
 };
+
 var createBoard = function() {
 	for (var i = 0; i < cards.length; i++) {
 		var cardElement = document.createElement( 'img' );
-		cardElement.setAttribute( 'src', 'images/back.png' );
-		cardElement.setAttribute( 'data-id', i );//[i]?
-		cardElement.addEventListener( 'click', flipCard );
-		document.getElementById('game-board').appendChild(cardElement);
+			cardElement.setAttribute( 'src', 'images/back.png' );
+			cardElement.setAttribute( 'data-id', i );//[i]?
+			cardElement.addEventListener( 'click', flipCard );
+			document.getElementById('game-board').appendChild(cardElement);
 	}
 };
 	
